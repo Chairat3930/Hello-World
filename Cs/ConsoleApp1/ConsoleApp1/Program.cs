589c80b1d5c31eff923aae27160a1c4d6cd25f52
Chairat2548 <chairat.wo@kkumail.com>
2024-11-22 18:30:07 -0800

    add

---
+++ Cs/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,7 @@
+﻿// See  https://aka.ms/new-console-template for more information
+string name = "Chairat Wonrattanachai";
+string student_id = "673450393-0";
+
+Console.WriteLine("Hello World");
+Console.WriteLine("My name is " + name);
+Console.WriteLine("My student id:" + student_id);
